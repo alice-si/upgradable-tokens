833d70199ec37f4ba0ce8224149e6c1723eef8c8
Jakub Wojciechowski <jakub@alice.si>
2017-07-18 14:55:35 +0100

    Babel language level

--- test/golem.js
+++ test/golem.js
@@ -2,12 +2,10 @@ var GolemToken = artifacts.require("./GolemNetworkToken.sol");
 
 contract('Golem Network Token', function(accounts) {
 
-  it("should deploy token with empty balance", function () {
-    return GolemToken.deployed().then(function (instance) {
-      return instance.totalSupply.call();
-    }).then(function (total) {
-      assert.equal(total.valueOf(), 0, "Total supply is different than 0");
-    });
+  it("should deploy token with empty balance", async function () {
+    let token = await GolemToken.deployed();
+    let total = await token.totalSupply.call();
+    assert.equal(total.valueOf(), 0, "Total supply is different than 0");
   });
 
 });
